9066fc8c17f8d3bdaa569a44870ffade0567046e
shumpei-kayano <shumpei.kayano@mail.o-hara.ac.jp>
2024-06-14 10:25:51 +0900

    リスト1.6 P42 許可するHTTPメソッドをGETとPOSTにする

--- apps/minimalapp/app.py
+++ apps/minimalapp/app.py
@@ -9,6 +9,6 @@ app = Flask(__name__)
 def index():
     return 'Hello, Flaskbook!'
 
-@app.route('/hello', methods=['GET'], endpoint='hello-endpoint')
+@app.route('/hello', methods=['GET', 'POST'], endpoint='hello-endpoint')
 def hello():
     return 'Hello, World!'
