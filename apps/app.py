80fb89219f99e1ec749cf3e06089c94453dc5dbe
shumpei-kayano <shumpei.kayano@mail.o-hara.ac.jp>
2024-06-21 10:22:39 +0900

    P96 SQLAlchemyをセットアップする

--- apps/app.py
+++ apps/app.py
@@ -1,9 +1,26 @@
+from pathlib import Path # 追加
 from flask import Flask
+from flask_migrate import Migrate # 追加
+from flask_sqlalchemy import SQLAlchemy # 追加
+
+# SQLAlchemyインスタンス生成
+db = SQLAlchemy()
 
 # create_app関数を作成する
 def create_app():
     # Flaskインスタンス生成
     app = Flask(__name__)
+    # アプリのコンフィグ設定をする
+    app.config.from_mapping(
+        SECRET_KEY='2AZSMss3p5QPbcY2hBj',
+        SQLALCHEMY_DATABASE_URI=f"sqlite:///{Path(__file__).parent / 'local.sqlite'}",
+        SQLALCHEMY_TRACK_MODIFICATIONS=False
+    )
+    
+    # SQLAlchemyとアプリを連携する
+    db.init_app(app)
+    # Migrateとアプリを連携する
+    Migrate(app, db)
     
     # crudパッケージからviewsをインポートする
     from apps.crud import views as crud_views
