2f3bc27f4679f85540cc497ff221f08d3a93e8f7
shumpei-kayano <shumpei.kayano@mail.o-hara.ac.jp>
2024-07-08 08:01:29 +0900

    P268 pytestのフィクスチャ

---
+++ tests/conftest.py
@@ -0,0 +1,5 @@
+import pytest
+
+@pytest.fixture
+def app_data():
+    return 3
--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,5 +1,16 @@
+import pytest
+
 def test_func1():
     assert 1 == 1
     
 def test_func2():
     assert 2 == 2
+    
+# @pytest.fictureを追加する
+# @pytest.fixture
+# def app_data():
+#     return 3
+
+# フィクスチャのk数を引数で指定すると関数の実行結果が渡される
+def test_func3(app_data):
+    assert app_data == 3
